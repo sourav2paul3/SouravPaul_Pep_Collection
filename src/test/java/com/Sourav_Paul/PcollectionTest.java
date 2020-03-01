05ea0284e62246e2dd5d9dd4893b580042289217
Sourav <Sourav2paul@gmail.com>
2020-03-01 14:16:18 +0530

    Pep_Collection

---
+++ src/test/java/com/Sourav_Paul/PcollectionTest.java
@@ -0,0 +1,14 @@
+package com.Sourav_Paul;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PcollectionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
